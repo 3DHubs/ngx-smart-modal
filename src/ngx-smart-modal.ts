5d46e6f92986045abfe25a67ab6aa52b55a93f75
jackobeney <jackobeney@googlemail.com>
2018-02-26 17:50:43 +0100

    just include dist

--- src/ngx-smart-modal.ts
+++
@@ -1,4 +0,0 @@
-// Public classes.
-export {NgxSmartModalService} from './services/ngx-smart-modal.service';
-export {NgxSmartModalComponent} from './components/ngx-smart-modal.component';
-export {NgxSmartModalModule} from './modules/ngx-smart-modal.module';
